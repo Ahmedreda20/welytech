a55ab6ad357b46b59c1a3ae74e4e5769f30639ce
Ahmedreda20 <62707388+Ahmedreda20@users.noreply.github.com>
2021-03-01 12:18:31 +0200

    first commit

--- js/main_all.js
+++ js/main_all.js
@@ -2,6 +2,7 @@ const btnmenu = document.querySelector('.btn_menu');
 const mdcontainer = document.querySelector('.md_sc_container');
 const searchbtn = document.querySelector('.btn_search');
 const searchcontainer = document.querySelector('.search_container');
+const auchorlinks = document.querySelectorAll('.normal_auchor  a');
 
 btnmenu.onclick = () => {
    btnmenu.classList.toggle('active_md');
@@ -16,5 +17,15 @@ searchcontainer.querySelector('.btn_close').onclick = e => {
    
    searchcontainer.querySelector('form').reset();
    searchcontainer.classList.remove('active_search');
-
 }
+
+
+auchorlinks.forEach(alink => {
+   var fullpath = window.location.href;
+   var index = fullpath.indexOf(alink.href);
+
+   if (index >= 0) {
+      alink.parentElement.classList.add('active_link');
+   }
+
+});
